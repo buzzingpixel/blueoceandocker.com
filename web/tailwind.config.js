067d2dbc07db226f2dc5b29ead160ad82caeb5f8
TJ Draper <tj@buzzingpixel.com>
2023-07-12 08:52:27 -0500

    Update tailwind settings

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,6 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
     content: [
         './app/**/*.{js,ts,jsx,tsx}',
         './pages/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    plugins: [
+        // Customize: https://github.com/tailwindlabs/tailwindcss-typography#customization
+        // https://github.com/tailwindlabs/tailwindcss-typography/blob/master/src/styles.js
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/aspect-ratio'),
     ],
 };
