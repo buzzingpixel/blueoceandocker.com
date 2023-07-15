31097b370b835dd69c78fa641611a29dcd689085
TJ Draper <tj@buzzingpixel.com>
2023-07-15 18:00:29 -0500

    Add the first lesson and null the second and third

--- web/app/Lessons.ts
+++ web/app/Lessons.ts
@@ -1,20 +1,20 @@
 export const Lessons = [
     {
-        name: 'Lesson 1: What Is Docker',
-        description: 'Learn about what Docker is and how it might benefit you!',
+        name: 'Lesson 1: Why Docker',
+        description: 'Learn about why you would want to use Docker and how it can benefit you!',
         href: '/',
-        youTubeId: 'toCycVBlW6k',
+        youTubeId: 'J3fk7fNMxGo',
     },
     {
         name: 'Lesson 2: Key Concepts',
         description: 'Learn about the important key concepts of Docker',
         href: '/lesson/key-concepts',
-        youTubeId: 'Se2_SaEfL-0',
+        youTubeId: null,
     },
     {
         name: 'Lesson 3: Running a Container',
         description: 'Learn how to set up and run a Docker container',
         href: '/lesson/running-a-container',
-        youTubeId: 'a3d9cK83gZQ',
+        youTubeId: null,
     },
 ];
